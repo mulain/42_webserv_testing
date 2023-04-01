72a3abd88483c1b3ed6da886f457f6398855da93
wolf <wolf.mardin@gmail.com>
2023-04-01 11:49:27 +0200

    auto add & push

--- src/main.cpp
+++ src/main.cpp
@@ -29,70 +29,16 @@
 	serverA.printRequest();
 } */
 
-#include <stdlib.h>
+
+
+
 #include <cstring>
-
-/* int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket;
-	long valread;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
-
-	(void)argv;
-	(void)argc;
-    
-    const char *hello = "Hello from server";
-    
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        perror("In socket");
-        exit(EXIT_FAILURE);
-    }
-    
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( 3000 );
-    
-    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-    
-    
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
-    {
-        perror("In bind");
-        exit(EXIT_FAILURE);
-    }
-    if (listen(server_fd, 10) < 0)
-    {
-        perror("In listen");
-        exit(EXIT_FAILURE);
-    }
-    while(1)
-    {
-        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-        {
-            perror("In accept");
-            exit(EXIT_FAILURE);
-        }
-        
-        char buffer[30000] = {0};
-        valread = read( new_socket , buffer, 30000);
-		(void)valread;
-        printf("%s\n",buffer );
-        write(new_socket , hello , strlen(hello));
-        printf("------------------Hello message sent-------------------\n");
-        close(new_socket);
-    }
-    return 0;
-} */
 
 int main()
 {
 	Socket	new_socket;
 	Server	servster;
+
 
 	while (1)
 	{
@@ -101,7 +47,8 @@ int main()
 		char buffer[30000] = {0};
 		read(newConnFd, buffer, 30000);
 		printf("%s\n",buffer );
-		write(newConnFd, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 5\r\n\r\nhello" , strlen("hello"));
+		const char *msg = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 17\r\n\r\nhello, u lil bish";
+		write(newConnFd, msg , strlen(msg));
 		printf("------------------Hello message sent-------------------\n");
 
 	}
